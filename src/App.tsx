ba6742d92dadaacce92577460d25fdcb2b4b7676
Prashan Pudasaini <prashan.pudasaini@outlook.com>
2022-11-29 20:20:16 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Hyperlink from '../@ui/Hyperlink'
 
 const App = () => {
     return (
         <>
             <h1>Welcome!</h1>
-            <Hyperlink href = "https://www.google.com">Go to google</Hyperlink>
         </>
     )
 }
